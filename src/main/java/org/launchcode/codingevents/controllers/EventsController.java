aa9e77362c4e9754713d61947a64841232e7b1f5
Srinidhi <srinidhi>
2021-01-17 14:20:51 -0600

    Code of thymeleaf and bootstrap exercise solution.

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -8,28 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/events")
 public class EventsController {
 
-    private static List<String> events = new ArrayList<>();
+    private static Map<String, String> events = new HashMap<String,String>();
 
     @GetMapping
     public String events(Model model){
+        events.put("Name1","Description1");
+        events.put("Name2","Description2");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    @GetMapping("create")
+    /*@GetMapping("create")
     public String renderCreateEventForm(){
        return "events/create";
     }
-
-    @PostMapping("create")
+*/
+    /*@PostMapping("create")
     public String createEvent(@RequestParam String eventName){
-         events.add(eventName);
+         events.put(eventName);
          return "redirect:";
-    }
+    }*/
 }
